b6eae9a3815be99cb0fdbc9da8a43e8bae8d9083
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-02-19 20:21:00 +0900

    roomモデルのインデント修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,16 +1,16 @@
 class Room < ApplicationRecord
-    has_one_attached :image
-    belongs_to :user
-    has_many :reservations, dependent: :destroy
+  has_one_attached :image
+  belongs_to :user
+  has_many :reservations, dependent: :destroy
 
-    validates :room_name, presence: true
-    validates :introduction, presence: true, length: {maximum: 100}
-    validates :price, numericality: true
-    validates :address, presence: true
-    validates :image, presence: true
+  validates :room_name, presence: true
+  validates :introduction, presence: true, length: {maximum: 100}
+  validates :price, numericality: true
+  validates :address, presence: true
+  validates :image, presence: true
 
-    def self.search(search)
-        where(["room_name like? or address like? or introduction like?", "%#{search}%", "%#{search}%", "%#{search}%"])
-    end
+  def self.search(search)
+      where(["room_name like? or address like? or introduction like?", "%#{search}%", "%#{search}%", "%#{search}%"])
+  end
 
 end
